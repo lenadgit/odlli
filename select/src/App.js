586613e1d0c195f1bd7be6e8adefa12ab7e9764b
Lena <lena.boruslav@gmail.com>
2019-03-29 13:49:48 +0200

    first push

---
+++ select/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <br/>
+          <h6>Стилизация Чекбокс</h6>
+ <input type="checkbox" id="check" name="check" />
+  <label for="check">Checkbox</label>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
